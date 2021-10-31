5b051cda20575884919ffaa55bd347ed7d90a0e5
Anubis <anubisawooo@163.com>
2021-10-31 09:34:43 +0800

    demo

---
+++ test/flutter_demo/lib/main01.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// 仅显示文本
+
+void main() {
+  runApp(
+    const Center(
+      child: Text(
+        'Hello, world!',
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
+}
